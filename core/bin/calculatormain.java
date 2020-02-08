8495bd8d5fe40c928e7654e9c5843313c8992fae
djzach <epicdjzach@gmail.com>
2020-02-08 11:36:36 -0500

    Create calculatormain.java

---
+++ core/bin/calculatormain.java
@@ -0,0 +1,15 @@
+public class calcMain extends program calculatorCore
+{
+
+  public void run () {
+    initScript();
+  }
+  
+  private void initScript 
+  {
+  System.out.prinln("Welcome to calcpro! An entry for the 2020 computer fair.");
+  }
+  
+
+
+}
